34de039d2d4f9826aeb813d0be7bf315a200c7d4
Artem <1582847232@qq.com>
2024-10-25 11:42:16 +0800

    no message

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,117 +1,117 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:phone_state/phone_state.dart';
-
-main() {
-  runApp(
-    const MaterialApp(
-      home: Example(),
-    ),
-  );
-}
-
-class Example extends StatefulWidget {
-  const Example({super.key});
-
-  @override
-  State<Example> createState() => _ExampleState();
-}
-
-class _ExampleState extends State<Example> {
-  PhoneState status = PhoneState.nothing();
-  bool granted = false;
-
-  Future<bool> requestPermission() async {
-    var status = await Permission.phone.request();
-
-    return switch (status) {
-      PermissionStatus.denied ||
-      PermissionStatus.restricted ||
-      PermissionStatus.limited ||
-      PermissionStatus.permanentlyDenied =>
-        false,
-      PermissionStatus.provisional || PermissionStatus.granted => true,
-    };
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isIOS) setStream();
-  }
-
-  void setStream() {
-    PhoneState.stream.listen((event) {
-      setState(() {
-        status = event;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Phone State'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (Platform.isAndroid)
-              MaterialButton(
-                onPressed: !granted
-                    ? () async {
-                        bool temp = await requestPermission();
-                        setState(() {
-                          granted = temp;
-                          if (granted) {
-                            setStream();
-                          }
-                        });
-                      }
-                    : null,
-                child: const Text('Request permission of Phone'),
-              ),
-            const Text(
-              'Status of call',
-              style: TextStyle(fontSize: 24),
-            ),
-            if (status.status == PhoneStateStatus.CALL_INCOMING ||
-                status.status == PhoneStateStatus.CALL_STARTED)
-              Text(
-                'Number: ${status.number}',
-                style: const TextStyle(fontSize: 24),
-              ),
-            Icon(
-              getIcons(),
-              color: getColor(),
-              size: 80,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  IconData getIcons() {
-    return switch (status.status) {
-      PhoneStateStatus.NOTHING => Icons.clear,
-      PhoneStateStatus.CALL_INCOMING => Icons.add_call,
-      PhoneStateStatus.CALL_STARTED => Icons.call,
-      PhoneStateStatus.CALL_ENDED => Icons.call_end,
-    };
-  }
-
-  Color getColor() {
-    return switch (status.status) {
-      PhoneStateStatus.NOTHING || PhoneStateStatus.CALL_ENDED => Colors.red,
-      PhoneStateStatus.CALL_INCOMING => Colors.green,
-      PhoneStateStatus.CALL_STARTED => Colors.orange,
-    };
-  }
-}
+// import 'dart:io';
+//
+// import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
+// import 'package:phone_state/phone_state.dart';
+//
+// main() {
+//   runApp(
+//     const MaterialApp(
+//       home: Example(),
+//     ),
+//   );
+// }
+//
+// class Example extends StatefulWidget {
+//   const Example({super.key});
+//
+//   @override
+//   State<Example> createState() => _ExampleState();
+// }
+//
+// class _ExampleState extends State<Example> {
+//   PhoneState status = PhoneState.nothing();
+//   bool granted = false;
+//
+//   Future<bool> requestPermission() async {
+//     var status = await Permission.phone.request();
+//
+//     return switch (status) {
+//       PermissionStatus.denied ||
+//       PermissionStatus.restricted ||
+//       PermissionStatus.limited ||
+//       PermissionStatus.permanentlyDenied =>
+//         false,
+//       PermissionStatus.provisional || PermissionStatus.granted => true,
+//     };
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (Platform.isIOS) setStream();
+//   }
+//
+//   void setStream() {
+//     PhoneState.stream.listen((event) {
+//       setState(() {
+//         status = event;
+//       });
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Phone State'),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             if (Platform.isAndroid)
+//               MaterialButton(
+//                 onPressed: !granted
+//                     ? () async {
+//                         bool temp = await requestPermission();
+//                         setState(() {
+//                           granted = temp;
+//                           if (granted) {
+//                             setStream();
+//                           }
+//                         });
+//                       }
+//                     : null,
+//                 child: const Text('Request permission of Phone'),
+//               ),
+//             const Text(
+//               'Status of call',
+//               style: TextStyle(fontSize: 24),
+//             ),
+//             if (status.status == PhoneStateStatus.CALL_INCOMING ||
+//                 status.status == PhoneStateStatus.CALL_STARTED)
+//               Text(
+//                 'Number: ${status.number}',
+//                 style: const TextStyle(fontSize: 24),
+//               ),
+//             Icon(
+//               getIcons(),
+//               color: getColor(),
+//               size: 80,
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   IconData getIcons() {
+//     return switch (status.status) {
+//       PhoneStateStatus.NOTHING => Icons.clear,
+//       PhoneStateStatus.CALL_INCOMING => Icons.add_call,
+//       PhoneStateStatus.CALL_STARTED => Icons.call,
+//       PhoneStateStatus.CALL_ENDED => Icons.call_end,
+//     };
+//   }
+//
+//   Color getColor() {
+//     return switch (status.status) {
+//       PhoneStateStatus.NOTHING || PhoneStateStatus.CALL_ENDED => Colors.red,
+//       PhoneStateStatus.CALL_INCOMING => Colors.green,
+//       PhoneStateStatus.CALL_STARTED => Colors.orange,
+//     };
+//   }
+// }
